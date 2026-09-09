c3c71c34de248b4b606eb461bbfa514ad115f436
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-09-09 11:51:11 +0200

    wire up app.py into main.py

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 import py_simple
 import webbrowser
 import threading
-from form.app import app  # <-- add this
+from form.app import app
 
 with open(
         "./template/job_description_template.md",
@@ -15,6 +16,11 @@ print(job_description)
 def open_browser():
     webbrowser.open("http://127.0.0.1:5000")
 
+
+
+
+
 if __name__ == "__main__":
-    threading.Timer(1, open_browser).start()
+    if os.environ.get("WERKZEUG_RUN_MAIN") == "true":
+        threading.Timer(1, open_browser).start()
     app.run(debug=True, port=5000)
